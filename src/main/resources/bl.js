88782a3a833a5d4e33f13f79c102979fa80aab76
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-06-04 13:05:13 +0300

    initial lines support

--- src/main/resources/bl.js
+++ src/main/resources/bl.js
@@ -1,6 +1,10 @@
-bthread('FiveHandler', 'Five.AllReady',
-    function (entity) {
-        bp.log.info("ZZ FiveHandler - requesting:")
-        bp.log.info(entity.center.map( cell => putInCol(cell.col, "Yellow")))
-        sync({request: entity.center.map( cell => putInCol(cell.col, "Yellow") )})
-    })
+let i = 0
+allFives.forEach(function (line) {
+    bthread('FiveHandler.' + i, 'Five.AllReady.' + i,
+        function (entity) {
+            bp.log.info("ZZ FiveHandler - requesting:")
+            bp.log.info(entity.center.map(cell => putInCol(cell.col, "Yellow")))
+            sync({request: entity.center.map(cell => putInCol(cell.col, "Yellow"))}, 80)
+        })
+    i++
+})
--- src/main/resources/dal.js
+++ src/main/resources/dal.js
@@ -1,5 +1,19 @@
 //TODO
-// let line = [{row: 5, col: 1, edge: true}, {row: 5, col: 2}, {row: 5, col: 3}, {row: 5, col: 4}, {row: 5, col: 5, edge: true}]
+// let line = [{row: 5, col: 1, edge: true}, {row: 5, col: 2}, {row: 5, col: 3, edge: true}]
+let allFives = [
+    [{row: 5, col: 1, edge: true}, {row: 5, col: 2}, {row: 5, col: 3, edge: true}],
+    [{row: 5, col: 2, edge: true}, {row: 5, col: 3}, {row: 5, col: 4, edge: true}]
+
+]
+
+// //TODO add more allFives
+// let allFives = []
+// for(var i = 0; i < 2; i++ ) {
+//     for(var j = 0; j < 3; j++) {
+//         allFives.push( [ { row : i, col : j, edge: true } , { row : i, col : j+1 } , { row : i, col : j+2 } , { row : i, col : j+3 } , { row : i, col : j+4 , edge: true} ] );
+//         allFives.push( [ { row : i, col : j, edge: true } , { row : i + 1, col : j } , { row : i + 2, col : j } , { row : i + 3, col : j }, { row : i + 4, col : j , edge: true} ] );
+//     }
+// }
 
 
 function buildCell(row, col) {
@@ -7,7 +21,7 @@ function buildCell(row, col) {
 }
 
 function buildLine(line) {
-    return 'line: ' + line.join()
+    return 'line: ' + line.map(cell => cell.row + ',' + cell.col).join("; ")
 }
 
 
@@ -26,37 +40,42 @@ ctx.registerEffect('BoardUpdated', function (data) {
         ctx.insertEntity(buildCell(ev_data.row - 1, ev_data.col), 'READY')
 
     // update lines
-    let all_ready = true
-    try {
-        for(var i = 0; i < line.length && all_ready; i++ ) {
-            let cell = line[i]
-            if (ctx.getEntityById(buildCell(cell.row, cell.col)).type != "READY") {
-                all_ready = false
-                bp.log.info("not ready")
-                bp.log.info(cell)
+    allFives.forEach(function (line) {
+        let all_ready = true
+        try {
+            for (var i = 0; i < line.length && all_ready; i++) {
+                let cell = line[i]
+                if (ctx.getEntityById(buildCell(cell.row, cell.col)).type != "READY") {
+                    all_ready = false
+                    bp.log.info("not ready")
+                    bp.log.info(cell)
+                }
             }
+        } catch (e) {
+            all_ready = false
         }
-    } catch (e) {
-        all_ready = false
-    }
-    try {
-        if (all_ready) {
-            ctx.insertEntity(buildLine(line), "ALL_READY", {center: line.filter((cell) => !cell.edge )})
-            bp.log.info("ZZ inserted: ")
-            bp.log.info(buildLine(line))
-        } else {
-            ctx.removeEntity(buildLine(line))
-            bp.log.info("ZZ removed: ")
-            bp.log.info(buildLine(line))
+        try {
+            if (all_ready) {
+                ctx.insertEntity(buildLine(line), "ALL_READY", {center: line.filter((cell) => !cell.edge)})
+                bp.log.info("ZZ inserted: ")
+                bp.log.info(buildLine(line))
+            } else {
+                ctx.removeEntity(buildLine(line))
+                bp.log.info("ZZ removed: ")
+                bp.log.info(buildLine(line))
+            }
+        } catch (e) {
+            // nothing to do
         }
-    } catch (e) {
-        // nothing to do
-    }
+    })
 })
 
-ctx.registerQuery('Five.AllReady',
-    entity => entity.id == buildLine(line))
-
+let i = 0
+allFives.forEach(function (line) {
+    ctx.registerQuery('Five.AllReady.' + i,
+        entity => entity.id == buildLine(line))
+    i++
+})
 
 
 
