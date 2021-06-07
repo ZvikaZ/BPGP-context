cdbeeef5710c0c844e5726ac6744630c8e8171ef
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-06-07 16:58:03 +0300

    janitorial

--- src/main/resources/bl.js
+++ src/main/resources/bl.js
@@ -1,29 +1,30 @@
 //region EventSets
 function AnyPutInCol(j) {
-  return bp.EventSet("Any put in column " + j, function (evt) {
-    return evt.name.equals("Put") && evt.data.col == j;
-  })
+    return bp.EventSet("Any put in column " + j, function (evt) {
+        return evt.name.equals("Put") && evt.data.col == j;
+    })
 }
 
 function AnyCoinInRow(i) {
-  return bp.EventSet("Any coin in row " + i, function (evt) {
-    return evt.name.equals("Coin") && evt.data.row == i;
-  })
+    return bp.EventSet("Any coin in row " + i, function (evt) {
+        return evt.name.equals("Coin") && evt.data.row == i;
+    })
 }
 
 function AnyCoinInFive(id, fiveCells) {
-  return bp.EventSet("Any coin in five " + id, function (e) {
-    if(!e.name.equals("Coin")) return false
-    for(let i=0; i<fiveCells.length; i++)
-      if(fiveCells[i].row==e.data.row && fiveCells[i].col==e.data.col)
-        return true
-  })
+    return bp.EventSet("Any coin in five " + id, function (e) {
+        if (!e.name.equals("Coin")) return false
+        for (let i = 0; i < fiveCells.length; i++)
+            if (fiveCells[i].row == e.data.row && fiveCells[i].col == e.data.col)
+                return true
+    })
 }
+
 //endregion
 
 //region Helper functions
 function Coin(row, col, color) {
-  return Event("Coin", {color: color, row: row, col: col});
+    return Event("Coin", {color: color, row: row, col: col});
 }
 
 //endregion
@@ -32,31 +33,31 @@ function Coin(row, col, color) {
 
 //region rules
 ctx.bthread("block put in full columns", "Column.All", function (column) {
-  for (let i = 0; i < ROWS; i++)
-    sync({waitFor: AnyPutInCol(column.col)})
-  sync({block: AnyPutInCol(column.col)})
+    for (let i = 0; i < ROWS; i++)
+        sync({waitFor: AnyPutInCol(column.col)})
+    sync({block: AnyPutInCol(column.col)})
 })
 
 ctx.bthread("put to coin", "Column.All", function (column) {
-  for (let i = 0; i < ROWS; i++) {
-    let color = sync({waitFor: AnyPutInCol(column.col)}).data.color
-    sync({request: Coin(i, column.col, color)})
-  }
+    for (let i = 0; i < ROWS; i++) {
+        let color = sync({waitFor: AnyPutInCol(column.col)}).data.color
+        sync({request: Coin(i, column.col, color)})
+    }
 })
 //endregion
 
 //region manage fives' state
 ctx.bthread("mark horizontal fives as ready", "Five.All", function (five) {
-  let count = 0
-  while(true) {
-    let col = sync({waitFor: AnyCoinInRow(five.cells[0].row - 1)})
-    if(col>=five.cells[0].col || col<=five.cells[5].col)
-      count++
-    if(count==5) {
-      sync({request: Event("Five is ready", five.id)})
-      return
+    let count = 0
+    while (true) {
+        let col = sync({waitFor: AnyCoinInRow(five.cells[0].row - 1)})
+        if (col >= five.cells[0].col || col <= five.cells[5].col)
+            count++
+        if (count == 5) {
+            sync({request: Event("Five is ready", five.id)})
+            return
+        }
     }
-  }
 })
 //endregion
 
--- src/main/resources/dal.js
+++ src/main/resources/dal.js
@@ -1,20 +1,20 @@
 //region Helper functions
 function buildCellId(row, col) {
-  return 'cell: ' + row + ", " + col
+    return 'cell: ' + row + ", " + col
 }
 
 function buildLine(line) {
-  return 'line: ' + line.map(cell => cell.row + ',' + cell.col).join("; ")
+    return 'line: ' + line.map(cell => cell.row + ',' + cell.col).join("; ")
 }
 
 //TODO move to official: ctx.has
 function ctx_has(id) {
-  try {
-    let entity = ctx.getEntityById(id)
-    return true
-  } catch (e) {
-    return false
-  }
+    try {
+        let entity = ctx.getEntityById(id)
+        return true
+    } catch (e) {
+        return false
+    }
 }
 
 //endregion
@@ -26,28 +26,28 @@ const cells = []
 const columns = []
 const fives = []
 for (let i = 0; i < ROWS - 5; i++) { // ACHIYA: These are not all fives...
-  for (let j = 0; j < COLS - 5; j++) {
-    let right = []
-    for (let k = 0; k < 5; k++) {
-      right.push({row: i, col: j + k, edge: k == 0 || k == 4 ? true : false})
+    for (let j = 0; j < COLS - 5; j++) {
+        let right = []
+        for (let k = 0; k < 5; k++) {
+            right.push({row: i, col: j + k, edge: k == 0 || k == 4 ? true : false})
+        }
+        fives.push(ctx.Entity("five right (" + i + "," + j + ")", "five", {
+            cells: right,
+            status: i == 0 ? "READY" : "NOT-READY"
+        }))
     }
-    fives.push(ctx.Entity("five right (" + i + "," + j + ")", "five", {
-      cells: right,
-      status: i == 0 ? "READY" : "NOT-READY"
-    }))
-  }
 }
 
 for (let j = 0; j < COLS; j++) {
-  columns.push(ctx.Entity("column: " + j, 'column', {col: j}))
-  for (let i = 0; i < ROWS; i++) {
-    cells.push(ctx.Entity(buildCellId(i, j), 'cell', {
-      row: i,
-      col: j,
-      color: "",
-      status: i == 0 ? "READY" : "NOT-READY"
-    }))
-  }
+    columns.push(ctx.Entity("column: " + j, 'column', {col: j}))
+    for (let i = 0; i < ROWS; i++) {
+        cells.push(ctx.Entity(buildCellId(i, j), 'cell', {
+            row: i,
+            col: j,
+            color: "",
+            status: i == 0 ? "READY" : "NOT-READY"
+        }))
+    }
 }
 
 ctx.populateContext(cells.concat(columns).concat(fives))
@@ -55,35 +55,35 @@ ctx.populateContext(cells.concat(columns).concat(fives))
 
 //region Queries
 ctx.registerQuery("Column.All", function (entity) {
-  return entity.type.equals("column")
+    return entity.type.equals("column")
 })
 ctx.registerQuery("Five.Ready", function (entity) {
-  return entity.type.equals("five") && entity.status.equals("READY")
+    return entity.type.equals("five") && entity.status.equals("READY")
 })
 ctx.registerQuery("Cell.All", function (entity) {
-  return entity.type.equals("cell")
+    return entity.type.equals("cell")
 })
 //endregion
 
 //region Effects
 ctx.registerEffect("Coin", function (data) {
-  let cell = ctx.getEntityById(buildCellId(data.row, data.col))
-  cell.color = data.color
-  ctx.updateEntity(cell)
-
-  if (data.row < ROWS - 1) {
-    let cellAbove = ctx.getEntityById(buildCellId(data.row + 1, data.col))
-    cellAbove.status = "READY"
+    let cell = ctx.getEntityById(buildCellId(data.row, data.col))
+    cell.color = data.color
     ctx.updateEntity(cell)
-  }
+
+    if (data.row < ROWS - 1) {
+        let cellAbove = ctx.getEntityById(buildCellId(data.row + 1, data.col))
+        cellAbove.status = "READY"
+        ctx.updateEntity(cell)
+    }
 
 // let fiveRight = ctx.getEntityById("")
 })
 
 ctx.registerEffect("Five is ready", function (data) {
-  const five = ctx.getEntityById(data)
-  five.status = "READY"
-  ctx.updateEntity(five)
+    const five = ctx.getEntityById(data)
+    five.status = "READY"
+    ctx.updateEntity(five)
 })
 //endregion
 
